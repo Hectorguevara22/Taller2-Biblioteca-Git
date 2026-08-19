1ce1528896e7d05448229acb9add2f0c76c244b5
hector <hjguevara@unicesar.edu.co>
2026-08-19 06:35:49 -0500

    feat: inicializar proyecto Maven de biblioteca

---
+++ src/main/java/com/mycompany/taller2/biblioteca/git/Taller2BibliotecaGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller2.biblioteca.git;
+
+/**
+ *
+ * @author Keiner
+ */
+public class Taller2BibliotecaGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
