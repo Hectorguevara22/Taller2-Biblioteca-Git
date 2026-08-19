70ee4eb53232967d3c85121695a1d3f6c8c92926
hector <hjguevara@unicesar.edu.co>
2026-08-19 07:02:07 -0500

    feat: implementar CREATE de Libro

--- src/main/java/com/mycompany/taller2/biblioteca/git/main.java
+++ src/main/java/com/mycompany/taller2/biblioteca/git/main.java
@@ -1,16 +1,36 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.taller2.biblioteca.git;
 
-/**
- *
- * @author Keiner
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
 
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<Libro> libros = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    }
+
+    public static Libro crearlibro() {
+        System.out.println("creacion de libros");
+        System.out.println("codigo");
+        String codigo = sc.nextLine();
+        System.out.println("titulo");
+        String titulo = sc.nextLine();
+        System.out.println("año de publicacion");
+        String yearpublic = sc.nextLine();
+        System.out.println("autor");
+        String autor = sc.nextLine();
+        boolean disponible = true;
+        Libro libro = new Libro(codigo, titulo, yearpublic, autor, disponible);
+        libros.add(libro);
+
+        System.out.println("Libro creado correctamente.");
+
+        return libro;
     }
 }
